dbc7838ee456ec37aeddb9a11b61524e8f656eb3
Daryl Cox <dcox@justmacapps.net>
2024-07-17 15:29:04 -0500

    Initial Commit

---
+++ JustASwiftLogTest1/JustASwiftLogTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  JustASwiftLogTest1App.swift
+//  JustASwiftLogTest1
+//
+//  Created by Daryl Cox on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct JustASwiftLogTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
