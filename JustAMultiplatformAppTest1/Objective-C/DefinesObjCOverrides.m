b959d9728f9d62b1dc904b5ea32bc6396923b116
Daryl R Cox <dcox@justmacapps.net>
2024-09-30 13:34:11 -0500

    NSLog override to XCGLogger working for multiple modules

--- JustAMultiplatformAppTest1/Objective-C/DefinesObjCOverrides.m
+++ JustAMultiplatformAppTest1/Objective-C/DefinesObjCOverrides.m
@@ -10,8 +10,6 @@
 
 #import "DefinesObjCOverrides.h"
 
-#import "JmObjCSwiftEnvBridge-Bridging-Header.h"
-
 @implementation DefinesObjCOverrides
 {
 
@@ -22,34 +20,29 @@
 - (void)initInstance
 {
 
-    NSLog(@"--- Defines.initInstance() - Invoked...");
-//  NSLogv(@"--- Defines.initInstance() - Invoked...", NULL);
+    NSLog(@"--- DefinesObjCOverrides.initInstance() - Invoked...");
 
     _jmObjCSwiftEnvBridge = JmObjCSwiftEnvBridge.sharedEnvBridge;
 
-//    NSLogv(@"--- Defines.initInstance() - Exiting - '_jmObjCSwiftEnvBridge' is [%@] ---", (__bridge struct __va_list_tag *)(_jmObjCSwiftEnvBridge));
-    NSLog(@"--- Defines.initInstance() - Exiting...");
+    NSLog(@"--- DefinesObjCOverrides.initInstance() - Exiting...");
 
 }
 
 - (void)customLoggerTest1:(NSString * _Nullable)message
 {
 
-    NSLog(@"--- Defines.customLoggerTest1() - Invoked ---");
-//  NSLogv(@"--- Defines.customLoggerTest1() - Invoked - Parameter 'message': %@", (__bridge struct __va_list_tag *)(message));
+    NSLog(@"--- DefinesObjCOverrides.customLoggerTest1() - Invoked ---");
     
     if (_jmObjCSwiftEnvBridge) {
-//        NSLogv(@"--- Defines.customLoggerTest1() - Test - '_jmObjCSwiftEnvBridge' is NOT Null - is [%@] ---", (__bridge struct __va_list_tag *)(_jmObjCSwiftEnvBridge));
         [_jmObjCSwiftEnvBridge jmLogMsg:message];
-        NSLog(@"--- Defines.customLoggerTest1() - Intermediate - parameter 'message' is [%@] ---", message);
+        NSLog(@"--- DefinesObjCOverrides.customLoggerTest1() - Intermediate - parameter 'message' is [%@] ---", message);
     } else {
-//      NSLogv(@"--- Defines.customLoggerTest1() - Test - '_jmObjCSwiftEnvBridge' is NULL - Error! ---", NULL);
-        NSLog(@"--- Defines.customLoggerTest1() - Test - '_jmObjCSwiftEnvBridge' is NULL - Error! ---");
+        NSLog(@"--- DefinesObjCOverrides.customLoggerTest1() - Test - '_jmObjCSwiftEnvBridge' is NULL - Error! ---");
     }
 
-    NSLog(@"--- Defines.customLoggerTest1() - Exiting ---");
-//  NSLogv(@"--- Defines.customLoggerTest1() - Exiting ---", NULL);
+    NSLog(@"--- DefinesObjCOverrides.customLoggerTest1() - Exiting ---");
 
 }
 
 @end
+
