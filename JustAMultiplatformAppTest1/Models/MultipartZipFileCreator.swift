d82d53f3b62402d3f697ef62e83062a8ae30a28b
Daryl R Cox <dcox@justmacapps.net>
2024-11-05 15:54:16 -0600

    Update MultipartZipFileCreator.swift

--- JustAMultiplatformAppTest1/Models/MultipartZipFileCreator.swift
+++ JustAMultiplatformAppTest1/Models/MultipartZipFileCreator.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 class MultipartZipFileCreator: NSObject
 {
@@ -51,7 +52,7 @@ class MultipartZipFileCreator: NSObject
     private func xcgLogMsg(_ sMessage:String)
     {
 
-        if (self.jmAppDelegateVisitor.bAppDelegateVisitorLogFilespecIsUsable == true)
+        if (self.jmAppDelegateVisitor.bForZipOperationsFilespecIsUsable == true)
         {
       
             self.jmAppDelegateVisitor.xcgLogMsg(sMessage)
@@ -78,111 +79,153 @@ class MultipartZipFileCreator: NSObject
 
         self.xcgLogMsg("\(sCurrMethodDisp) Invoked - parameter 'multipartRequestInfo' is [\(multipartRequestInfo.toString())]...")
 
-        var urlForZipOperations:URL = URL(string:multipartRequestInfo.sAppSourceFilespec)!
+        var urlCreatedZipFile:URL?                    = nil     // URL of the Zip file (created)...
+        var urlForZipOperationsSource:URL?            = URL(string:multipartRequestInfo.sAppSourceFilespec)!
+        var urlForZipOperationsTarget:URL?            = URL(string:multipartRequestInfo.sAppZipFilename)!
+        var sForZipOperationsSourceFilespec:String    = self.urlForZipOperationsSource?.path
+        var sForZipOperationsTargetFilename:String    = self.urlForZipOperationsTarget?.path
 
-        if (self.bGenerateInternalTextFiles == true) 
+        // Check that we have a 'target' file (string) that is NOT nil...
+
+        if (sForZipOperationsTargetFilespec.count < 1)
         {
 
-            urlForZipOperations = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            self.xcgLogMsg("[\(sCurrMethodDisp)] Unable to Zip the 'source' filespec of [\(String(describing: sForZipOperationsSourceFilespec))] - the 'target' Zip filename is 'nil' - Error!")
 
-            // Populate the App's 'documents' directory with some example files:
+            // Exit:
 
-            for i in 1...10 
+            urlCreatedZipFile = nil
+
+            self.xcgLogMsg("\(sCurrMethodDisp) Exiting - 'urlCreatedZipFile' is [\(String(describing: urlCreatedZipFile))]...")
+
+            return urlCreatedZipFile
+
+        }
+
+        // Check that we have a 'source' file to zip (or if 'testing' make sure we have one)...
+
+        let bIsForZipOperationsSourceFilePresent:Bool = JmFileIO.fileExists(sFilespec:self.sForZipOperationsSourceFilespec)
+
+        if (bIsForZipOperationsSourceFilePresent == true)
+        {
+
+            self.xcgLogMsg("[\(sCurrMethodDisp)] Preparing to Zip the 'source' filespec of [\(String(describing: sForZipOperationsSourceFilespec))]...")
+
+        }
+        else
+        {
+
+            if (self.bGenerateInternalTextFiles == false)
+            {
+                
+                self.xcgLogMsg("[\(sCurrMethodDisp)] Unable to Zip the 'source' filespec of [\(String(describing: sForZipOperationsSourceFilespec))] - the file does NOT Exist - Error!")
+
+                // Exit:
+
+                urlCreatedZipFile = nil
+
+                self.xcgLogMsg("\(sCurrMethodDisp) Exiting - 'urlCreatedZipFile' is [\(String(describing: urlCreatedZipFile))]...")
+
+                return urlCreatedZipFile
+
+            } 
+            else
             {
 
-                let fileUrl   = urlForZipOperations.appendingPathComponent("\(i).txt")
-                let sContents = "This is file #(\(i))..."
+                do 
+                {
 
-                try! sContents.write(to: fileUrl, atomically: true, encoding: .utf8)
+                    var urlForZipOperationsFilepath = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask ,appropriateFor: nil, create: true)
+                    var urlForZipOperationsFilespec = urlForZipOperationsFilepath?.appendingPathComponent("Test-for-Zip_1.txt")
+                    var sForZipOperationsFilespec   = urlForZipOperationsFilespec?.path
+                    var sForZipOperationsFilepath   = urlForZipOperationsFilepath?.path
+
+                    self.xcgLogMsg("[\(sCurrMethodDisp)] 'self.sForZipOperationsFilespec' (computed) is [\(String(describing: self.sForZipOperationsFilespec))]...")
+                    self.xcgLogMsg("[\(sCurrMethodDisp)] 'self.sForZipOperationsFilepath' (resolved #1) is [\(String(describing: self.sForZipOperationsFilepath))]...")
+
+                    let bIsForZipOperationsFilePresent:Bool = JmFileIO.fileExists(sFilespec:sForZipOperationsFilespec)
+
+                    if (bIsForZipOperationsFilePresent == true)
+                    {
+
+                        self.xcgLogMsg("[\(sCurrMethodDisp)] Zipping the 'source' Filespec of [\(String(describing: sForZipOperationsFilespec))]...")
+
+                        sForZipOperationsSourceFilespec = sForZipOperationsFilespec
+
+                    }
+                    else
+                    {
+
+                        try FileManager.default.createDirectory(atPath: self.sForZipOperationsFilepath, withIntermediateDirectories: true, attributes: nil)
+
+                        let sGeneratedFileContents:String = "\(sCurrMethodDisp) Invoked - 'sApplicationName' is [\(AppGlobalInfo.sApplicationName)] - 'self' is [\(self)]...\r\nThis is an 'auto' generated file used for 'testing'..."
+
+                        try sGeneratedFileContents.write(toFile: self.sForZipOperationsFilespec, atomically:true, encoding:String.Encoding.utf8)
+
+                        self.xcgLogMsg("[\(sCurrMethodDisp)] Successfully created the 'path' of [.documentDirectory] and the 'source' filespec of [\(String(describing: self.sForZipOperationsFilespec))]...")
+
+                        sForZipOperationsSourceFilespec = sForZipOperationsFilespec
+
+                    }
+
+                }
+                catch
+                {
+
+                    self.xcgLogMsg("[\(sCurrMethodDisp)] Failed to create the 'path' of [.documentDirectory] - Details: \(error) - Error!")
+
+                    // Exit:
+
+                    urlCreatedZipFile = nil
+
+                    self.xcgLogMsg("\(sCurrMethodDisp) Exiting - 'urlCreatedZipFile' is [\(String(describing: urlCreatedZipFile))]...")
+
+                    return urlCreatedZipFile
+
+                }
 
             }
 
         }
 
-        // Check the accessibility of the 'urlForZipOperations'...
+        // Zip the 'source' file to the 'target' Zip file...
 
-        self.xcgLogMsg("\(sCurrMethodDisp) Creating a Zip file from the URL 'urlForZipOperations' of [\(urlForZipOperations)]...")
+        self.xcgLogMsg("[\(sCurrMethodDisp)] Zipping the 'source' filespec of [\(String(describing: sForZipOperationsSourceFilespec))] to the 'target' Zip filename of [\(sForZipOperationsTargetFilename)]...")
 
-    //  var sForZipOperationsIsFile:Bool      = false
-    //  var sForZipOperationsIsDirectory:Bool = false
-    //  let sForZipOperations:String          = urlAppDelegateVisitorLogFilespec?.path
-    //
-    //  let nsDictItemAttributes:NSDictionary? = try? FileManager.default.attributesOfItem(atPath: sForZipOperations) as NSDictionary
-    //
-    //  if (nsDictItemAttributes == nil)
-    //  {
-    //
-    //      // Exit:
-    //
-    //      self.xcgLogMsg("\(sCurrMethodDisp) Exiting - Can NOT determine the attributes of 'urlCreatedZipFile' of [\(String(describing: urlCreatedZipFile))] - Error!")
-    //
-    //      return nil
-    //
-    //  }
-    //
-    //  if (nsDictItemAttributes!.fileType() = "NSFileTypeDirectory")
-    //  {
-    //
-    //      sForZipOperationsIsFile      = false
-    //      sForZipOperationsIsDirectory = true
-    //
-    //      self.xcgLogMsg("\(sCurrMethodDisp) The URL 'urlForZipOperations' of [\(urlForZipOperations)] is a 'directory'...")
-    //
-    //  }
-    //  else
-    //  {
-    //
-    //      sForZipOperationsIsFile      = true
-    //      sForZipOperationsIsDirectory = false
-    //
-    //      self.xcgLogMsg("\(sCurrMethodDisp) The URL 'urlForZipOperations' of [\(urlForZipOperations)] is a 'file'...")
-    //
-    //  }
+        do
+        {
 
+            let urlForSourceFilespec:URL? = URL(string:sForZipOperationsSourceFilespec)!
+            let urlForTargetZipFile:URL?  = FileManager.default.temporaryDirectoryappendingPathComponent(sForZipOperationsTargetFilename)
+            let zipArchiver:ZipArchive    = ZipArchive()
 
+            try zipArchiver.createZipFile(at:urlForTargetZipFile, withContentsOf:urlForSourceFilespec, options:.default)
 
-        var urlCreatedZipFile:URL?              = nil                   // URL of the Zip file (created)...
-        var nsError:NSError?                    = nil                   // Store an encountered 'NSError'...
-        let nsFileCoordinator:NSFileCoordinator = NSFileCoordinator()
+            multipartRequestInfo.dataAppFile      = try Data(contentsOf:urlForTargetZipFile)
+            multipartRequestInfo.sAppFileMimeType = "application/zip"
 
-        // Zip the documents directory
-        // This block is synchronous and the block will be executed before it returns
-        // If the method fails, the block will not be executed though
-        // If you expect the archiving process to take long, execute it on another queue:
+            urlCreatedZipFile                     = urlForTargetZipFile
 
-        nsFileCoordinator.coordinate(readingItemAt:urlForZipOperations, options:[.forUploading], error:&nsError) 
-        { (urlFileToAddToZip) in
+        }
+        catch
+        {
 
-            self.xcgLogMsg("\(sCurrMethodDisp) NSFileCoordinator: 'urlFileToAddToZip' is [\(urlFileToAddToZip)]...")
+            self.xcgLogMsg("[\(sCurrMethodDisp)] Failed to create the 'target' Zip file [\(sForZipOperationsTargetFilename)] - Error!")
 
-            // 'urlFileToAddToZip' points to the file iterated by the 'nsFileCoordinator' and
-            // is valid ONLY until the end of this block, so the file MUST be moved to a temporary folder:
+            // Exit:
 
-            let urlTempFile = try! FileManager.default.url(for:            .itemReplacementDirectory,
-                                                           in:             .userDomainMask,
-                                                           appropriateFor: urlFileToAddToZip,
-                                                           create:         true
-                                                           )
-                                                           .appendingPathComponent(multipartRequestInfo.sAppZipFilename)
+            urlCreatedZipFile = nil
 
-            self.xcgLogMsg("\(sCurrMethodDisp) URL 'urlFileToAddToZip' of [\(urlFileToAddToZip)] created the 'urlTempFile' of [\(urlTempFile)]...")
+            self.xcgLogMsg("\(sCurrMethodDisp) Exiting - 'urlCreatedZipFile' is [\(String(describing: urlCreatedZipFile))]...")
 
-            try! FileManager.default.moveItem(at:urlFileToAddToZip, to:urlTempFile)
-
-            self.xcgLogMsg("\(sCurrMethodDisp) URL 'urlFileToAddToZip' of [\(urlFileToAddToZip)] moved to the 'urlTempFile' of [\(urlTempFile)]...")
-
-            // Store the URL so we can use it outside the block:
-
-            urlCreatedZipFile = urlTempFile
+            return urlCreatedZipFile
 
         }
 
+        // Check if we actually got the 'target' Zip file created...
+
         if let urlCreatedZipFile = urlCreatedZipFile 
         {
-
-        // Bring up the 'share' sheet so we can send the 'urlTargetZipFile' with AirDrop (for example):
-        //     let avc = UIActivityViewController(activityItems: [urlCreatedZipFile], applicationActivities: nil)
-        //     present(avc, animated: true)
 
             self.xcgLogMsg("\(sCurrMethodDisp) Produced a Zip file 'urlCreatedZipFile' of [\(urlCreatedZipFile)]...")
 
@@ -190,7 +233,9 @@ class MultipartZipFileCreator: NSObject
         else 
         {
 
-            self.xcgLogMsg("\(sCurrMethodDisp) Failed to produce a Zip file - 'nsError' is [\(String(describing: nsError))] - the supplied URL 'urlForZipOperations' was [\(urlForZipOperations)] - Error!")
+            self.xcgLogMsg("\(sCurrMethodDisp) Failed to produce a Zip file - the 'target' Zip filename was [\(sForZipOperationsTargetFilename)] - Error!")
+
+            urlCreatedZipFile = nil
 
         }
 
